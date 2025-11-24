6d29b140c227942a95c5d9df82cd75769a63580e
Preetanshu Mishra <preetanshumishra@gmail.com>
2025-11-24 10:38:57 -0400

    Fixed Status Bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 import { RootNavigator } from '../navigation/RootNavigator';
+import { useTheme } from '../src/hooks/useTheme';
 
 export default function RootLayout() {
+  const { theme } = useTheme();
+
   return (
     <>
       <RootNavigator />
-      <StatusBar style="auto" />
+      <StatusBar style={theme === 'dark' ? 'light' : 'dark'} />
     </>
   );
 }
